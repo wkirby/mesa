dafac698a32b99beeed7fa51f0cad54abe6c21ba
Wyatt Kirby <kirby.wa@gmail.com>
2014-02-09 00:24:48 -0800

    Add Timezone Check to Security

--- mesa/security.php
+++ mesa/security.php
@@ -5,3 +5,8 @@ if (!defined('MESA')) { die('Hackerz OMG'); }
 
 // Verify PHP Version
 if ( floatval(phpversion()) < 5.4) { die('Please install PHP 5.4 or newer.'); }
+
+// Set Timezone
+if (!ini_get('date.timezone')) {
+	date_default_timezone_set('America/Los_Angeles');
+}
